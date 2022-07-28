06c13865db13b249a8d7d242b2f1277d460722de
TakamiChie@Ardha <chie@onpu-tamago.net>
2022-07-28 17:07:13 +0900

    今後の拡張に備えてIDの変更

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const defaultValues = {
-  "url": "http://example.com/",
-  "zoom": 1.0,
-  "xadjust": 50,
-  "yadjust": 50
+  "instagram_url": "http://example.com/",
+  "instagram_zoom": 1.0,
+  "instagram_xadjust": 50,
+  "instagram_yadjust": 50
 }
 
 
@@ -15,42 +15,43 @@ window.addEventListener("load", (e) => {
   });
 });
 
-document.getElementById("base").addEventListener("dragover", (e) => {
-  e.stopPropagation();
-  e.preventDefault();
-  e.dataTransfer.dropEffect = "copy";
-}, false);
+Array.from(document.getElementsByClassName("base")).forEach((base) => {
+  base.addEventListener("dragover", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "copy";
+  }, false);
+  base.addEventListener("drop", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
 
-document.getElementById("base").addEventListener("drop", (e) => {
-  e.stopPropagation();
-  e.preventDefault();
-
-  let file = e.dataTransfer.files[0];
-  document.getElementById("img").src = URL.createObjectURL(file);
+    let file = e.dataTransfer.files[0];
+    base.querySelector("img[data-role='image']").src = URL.createObjectURL(file);
+  });
 });
 
-document.getElementById("url").addEventListener("change", (e) => {
+document.getElementById("instagram_url").addEventListener("change", (e) => {
   refresh();
 });
 
-Array.from(document.querySelectorAll("input[type=range]")).forEach(e => e.addEventListener("input", refreshzoom));
+Array.from(document.querySelectorAll("#SBCast_instagram_content input[type=range]")).forEach(e => e.addEventListener("input", refreshzoom));
 
-document.getElementById("resetpos").addEventListener("click", (e) => {
-  Array.from(document.querySelectorAll("input[type=range]")).forEach((e) => {
+document.getElementById("instagram_resetpos").addEventListener("click", (e) => {
+  Array.from(document.querySelectorAll("#SBCast_instagram_content input[type=range]")).forEach((e) => {
     e.value = defaultValues[e.id];
     localStorage.setItem(e.id, e.value);
   });
   refreshzoom();
 });
 
-document.getElementById("update").addEventListener("click", (e) => {
+document.getElementById("instagram_update").addEventListener("click", (e) => {
   refresh();
 });
 
-document.getElementById("save").addEventListener("click", async (e) => {
+document.getElementById("instagram_save").addEventListener("click", async (e) => {
   refresh();
   const dl = document.createElement("a");
-  const b = document.getElementById("base");
+  const b = document.getElementById("instagram_base");
   dl.href = await domtoimage.toPng(b, {
     width: b.clientWidth,
     height: b.clientHeight
@@ -61,18 +62,18 @@ document.getElementById("save").addEventListener("click", async (e) => {
 
 function refreshzoom() {
   const param = {};
-  ["zoom", "xadjust", "yadjust"].forEach(v => param[v] = document.getElementById(v).value);
+  ["zoom", "xadjust", "yadjust"].forEach(v => param[v] = document.getElementById(`instagram_${v}`).value);
   console.log(`translate(${param['xadjust'] * -1}%, ${param['yadjust'] * -1}%) scale(${param['zoom']})`);
-  document.getElementById("img").style.transform = `translate(${param['xadjust'] * -1}%, ${param['yadjust'] * -1}%) scale(${param['zoom']})`;
+  document.getElementById("instagram_img").style.transform = `translate(${param['xadjust'] * -1}%, ${param['yadjust'] * -1}%) scale(${param['zoom']})`;
 }
 
 function refresh() {
-  const url = document.getElementById("url").value;
-  const qr = document.getElementById("qr");
+  const url = document.getElementById("instagram_url").value;
+  const qr = document.getElementById("instagram_qr");
   qr.innerHTML = "";
   new QRCode(qr, {
     text: url,
-    width: document.getElementById("base").clientHeight / 5,
-    height: document.getElementById("base").clientHeight / 5
+    width: document.getElementById("instagram_base").clientHeight / 5,
+    height: document.getElementById("instagram_base").clientHeight / 5
   });
 }
